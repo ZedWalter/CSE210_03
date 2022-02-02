50614b60838fbea2abfc5d76b67be840d30250dd
ZedWalter <wal20005@byui.edu>
2022-02-02 10:52:25 -0700

    Filled in Word class

--- game/word.py
+++ game/word.py
@@ -1,9 +1,28 @@
 import random
 
-class Card:
+class Word:
     """
     The responsibility of word is to initialize an array with a list of words and pick a random word
 
     Attributes:
-        value (int): The value of the card drawn, between 1 and 13.
+        
     """
+    word_list = []
+
+    # This function initializes our word class with an empty array.
+    def __innit__(self):
+        self.word_list = []
+
+    # This function appends the list of words you want to use to your list.
+    def add_word(self):
+        #adding some words to intially help test our display program
+        test_list = ['pasta', 'ramen', 'steak', 'apple', 'lemon', 'onion', 'snail']
+        self.word_list.extend(test_list)
+
+    # This function grabs a random word from the list and returns it.
+    def get_word(self):
+        length = len(self.word_list)
+        word = self.word_list[random.randint(0,length - 1)]
+
+        return word
+
