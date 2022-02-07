151ab0d0d839cb94439a0b9ab28fccdd33655f91
Protomanzero0 <protomanzero01@gmail.com>
2022-02-07 09:52:59 -0700

    testing display formatting

--- game/display.py
+++ game/display.py
@@ -4,3 +4,38 @@ class Display:
     The responsibility of Display is to create and display the picture to the game.
     
     """
+
+    def __init__(self):
+        self._body = """  /|\
+                          / \
+                """
+        self._line = 0
+        self.head = "o"
+        self._parachute = [
+        "  ___  \n",
+        " / ___ \ \n",
+        " \     / \n",
+        "  \   /  \n", 
+        self.head
+        ]
+
+
+    def parachute(self, lives):
+        self._line = lives
+        for _ in range(self._line, 5):
+            self._line += 1
+
+    def print_man(self):
+        print(self._parachute + self._body)
+
+
+
+
+
+#   ___
+# / ___ \
+# \     /
+#  \   /
+#    o
+#   /|\
+#   / \
