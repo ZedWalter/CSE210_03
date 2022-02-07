1f0a8c086737cdcb63d36c7f2c3afcdf607584d9
ZedWalter <wal20005@byui.edu>
2022-02-07 09:53:25 -0700

    updated player with starting functions

--- game/display.py
+++ game/display.py
@@ -4,3 +4,4 @@ class Display:
     The responsibility of Display is to create and display the picture to the game.
     
     """
+
--- game/player.py
+++ game/player.py
@@ -1,3 +1,5 @@
+from word import Word
+
 class Player:
     """One who plays the game.
     
@@ -7,3 +9,18 @@ class Player:
         
         
     """
+    def __init__(self):
+        self.word_list = []
+
+    def _split_word(self):
+        word = Word()
+        game_word = word.get_word()
+        self.word_list = list(game_word)
+
+
+    def display_empty_word(self):
+        empty_array = []
+        for I in range(1, len(self.word_list)):
+            empty_array.append('_')
+        
+        return empty_array
