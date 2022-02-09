4a913f5dedc05ec7e69af842ce86873d10824a8b
MagnesiumSail <91155989+MagnesiumSail@users.noreply.github.com>
2022-02-09 09:55:49 -0700

    Added comment for testing

--- game/director.py
+++ game/director.py
@@ -3,3 +3,10 @@ class Director:
     
     The responsibility of the Director is to control the program event order.
     """
+    def __init__(self):
+        """Constructs a new Director instance.
+        Far
+        Args:
+            self(Director): an instance of the Director
+        """
+        self.playing = True
