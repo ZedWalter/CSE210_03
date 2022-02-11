70ccf5002c55f57f9083dea2eef757cf3661af42
Protomanzero0 <protomanzero01@gmail.com>
2022-02-10 21:30:21 -0700

    Parachute displays correctly, end game behavior works

--- game/display.py
+++ game/display.py
@@ -9,31 +9,40 @@ class Display:
         self._body = """   /|\\
    / \\
                 """
+    
         self._line = 0
         self.head = "    o    "
+        self._newparachute = []
         self._parachute = [
-        "   ___  ",
-        " / ___ \ ",
-        " \     / ",
-        "  \   /  ", 
-        self.head
+        "   ___  ", #0
+        " / ___ \ ", #1
+        " \     / ", #2
+        "  \   /  ", #3
+        self.head #4
         ]
 
 
     def parachute(self, lives):
-        self._line = lives
-        for _ in range(self._line, 5):
-            self._line += 1
+        if lives == 0:
+            self.head = "    x"
+            self._parachute[4] = self.head
+            self._newparachute = self._parachute[4:]
+            # print (self._parachute)
+            return self._newparachute
 
+        self._newparachute = self._parachute[-1*(lives-4):]
+        
+        return self._newparachute
+
+    
     def print_man(self):
-        for x in self._parachute:
+        for x in self._newparachute:
             print(x)
         print(self._body)
 
-dsp = Display()
-dsp.print_man()
-
-
+# dsp = Display()
+# dsp.parachute()
+# dsp.print_man()
 
 #   ___
 # / ___ \
