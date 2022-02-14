5dbfb1c359c3a39200e90ee2a40dfadf4b3f92f6
MagnesiumSail <91155989+MagnesiumSail@users.noreply.github.com>
2022-02-14 09:48:30 -0700

    Testing Push

--- game/director.py
+++ game/director.py
@@ -1,12 +1,35 @@
+from game.word import Word
+from game.display import Display
+from game.player import Player
+
 class Director:
     """A person who directs the game.
     
     The responsibility of the Director is to control the program event order.
     """
+    
     def __init__(self):
         """Constructs a new Director instance.
-        Far
+        Far 
         Args:
             self(Director): an instance of the Director
         """
-        self.playing = True
+        self._playing = True
+        self.grab_input = None
+    def start_game(self):
+        
+        disp = Display()
+        palp = Player()
+        disp.parachute(self, palp.return_lives)
+        disp.print_man()
+        while self.playing:
+            palp.get_input
+            palp.create_display_list()
+            disp.parachute(self, palp.return_lives)
+            disp.print_man()
+            if palp.lives == 0:
+                self.grab_input = input("Would you like to play again?: [y/n]")
+                if self.grab_input == "y":
+                    self.playing = True
+                else:
+                    self.playing = False
